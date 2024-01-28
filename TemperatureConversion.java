a1c60fd7445972906f5aecb85ee3a552fd18f6a7
cincy sebastian <cincy123@gmail.com>
2024-01-28 13:03:29 -0700

    TC

--- TemperatureConversion.java
+++ TemperatureConversion.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class TemperatureConversion {
+public class TemperatureConversion {
+    public  static void main (String[] args){
+        //Printing the name of the project
+        System.out.println("Temperature Conversion Project");
+        //get the input for temperature in Fahrenheit
+        System.out.println("Enter the temperature in Fahrenheit: ");
+        //Assigning the temperature value to a variable
+        double temperature=60;
+        //Converting the temperature in Fahrenheit to Celsius
+        double TempCelsius=(temperature-32)*5/9;
+        //Printing temperature in Celsius
+        System.out.println("The temperature in Celsius is: "+TempCelsius);
+        //Converting the temperature in Fahrenheit to Kelvin
+        double TempKelvin= (float) (TempCelsius+273.15);
+        //Printing temperature in Kelvin
+        System.out.println("The temperature in Kelvin is: "+TempKelvin);
+    }
 }
