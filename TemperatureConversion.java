45ff48587d72cd048de9d9bc44dba7eae66914a3
cincy <cincy123@gmail.com>
2024-01-29 17:59:25 -0700

    TC1

--- TemperatureConversion.java
+++ TemperatureConversion.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
 public class TemperatureConversion {
     public  static void main (String[] args){
         //Printing the name of the project
         System.out.println("Temperature Conversion Project");
         //get the input for temperature in Fahrenheit
+        Scanner temp=new Scanner(System.in);
         System.out.println("Enter the temperature in Fahrenheit: ");
         //Assigning the temperature value to a variable
-        double temperature=60;
+        double temperature=temp.nextDouble();
+        System.out.println("Temperature in Fahrenheit: "+temperature);
         //Converting the temperature in Fahrenheit to Celsius
         double TempCelsius=(temperature-32)*5/9;
         //Printing temperature in Celsius
